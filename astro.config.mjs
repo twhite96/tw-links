2dc822fdec2293378089c809c5c591d343151ea3
Tiffany <hey@tiffanyrwhite.com>
2023-01-19 21:26:10 -0500

    (no branch): (no branch):

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  // ...
+  site: "http://localhost:3000",
   integrations: [tailwind()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
+  theme: {
+    colors: {
+      blue: {
+        500: "#0A66C2",
+      },
+      orange: "#F58025",
+      black: "#181717",
+    },
+    extend: {},
+  },
+
+  plugins: [],
+};
+  // darkMode: "class"
+
